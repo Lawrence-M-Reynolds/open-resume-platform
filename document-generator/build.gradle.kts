1dc478521b1d2f17110e381d5d7e1ba90eef7f72
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2026-02-12 13:21:10 +0000

    Basic app created.

---
+++ document-generator/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.reynolds"
+version = "0.0.1-SNAPSHOT"
+description = "A containerized Spring Boot & React platform for dynamic CV generation. Features a microservice architecture using Pandoc for high-fidelity PDF/Docx export and modular content versioning."
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
