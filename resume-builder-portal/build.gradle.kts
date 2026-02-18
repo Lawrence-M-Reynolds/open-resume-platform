404a70b6bd875772c1286fef640a6eeb44d07d22
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2026-02-18 16:25:55 +0000

    Added a root gradle build file and refactored them all.

--- resume-builder-portal/build.gradle.kts
+++ resume-builder-portal/build.gradle.kts
@@ -1,44 +1,19 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.0.2"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-group = "com.reynolds"
-version = "0.0.1-SNAPSHOT"
-description = "A containerized Spring Boot & React platform for dynamic CV generation. Features a microservice architecture using Pandoc for high-fidelity PDF/Docx export and modular content versioning."
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
+	id("org.springframework.boot")
 }
 
 dependencies {
 	implementation(project(":open-resume-lib"))
 
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	compileOnly("org.projectlombok:lombok")
-	//developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	implementation("org.springframework.boot:spring-boot-starter-restclient")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
