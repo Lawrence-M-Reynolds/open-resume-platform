4f5a0a22f2c8d6481e3423e4222ee0d19900d264
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2026-02-19 22:04:22 +0000

    setting up railway config.

--- document-generator-gateway/build.gradle.kts
+++ document-generator-gateway/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-restclient")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation(libs.springdoc.ui)
 
 	compileOnly("org.projectlombok:lombok")
