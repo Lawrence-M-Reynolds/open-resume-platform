b70603896453c39f8173e21ea078796cb85ce9a2
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2026-02-21 15:05:59 +0000

    CORS fix for railway on the front end.

--- resume-builder-portal/src/main/java/com/reynolds/open_resume_platform/config/GlobalCorsConfig.java
+++ resume-builder-portal/src/main/java/com/reynolds/open_resume_platform/config/GlobalCorsConfig.java
@@ -17,7 +17,7 @@ public class GlobalCorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // 1. Allow your Frontend Origin
-        config.setAllowedOrigins(Arrays.asList("http://localhost", "http://localhost:80", "http://localhost:5173"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost", "http://localhost:80", "http://localhost:5173", "https://front-end-production-e558.up.railway.app"));
 
         // 2. Allow all common methods
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
