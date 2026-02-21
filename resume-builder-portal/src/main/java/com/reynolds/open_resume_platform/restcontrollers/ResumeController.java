034a235bbd637bd83298085dcae64326c4155d6e
Negra Hodzic <negra.hodzic@gmail.com>
2026-02-21 01:29:00 +0000

    Added POST /api/v1/resumes endpoint

---
+++ resume-builder-portal/src/main/java/com/reynolds/open_resume_platform/restcontrollers/ResumeController.java
@@ -0,0 +1,27 @@
+package com.reynolds.open_resume_platform.restcontrollers;
+
+import com.reynolds.open_resume_platform.resumes.command.CreateResumeCommand;
+import com.reynolds.open_resume_platform.resumes.domain.Resume;
+import com.reynolds.open_resume_platform.resumes.service.ResumeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/resumes")
+public class ResumeController {
+
+    private final ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Resume> create(@RequestBody CreateResumeCommand command) {
+        Resume resume = resumeService.create(command);
+        return ResponseEntity.status(201).body(resume);
+    }
+}
